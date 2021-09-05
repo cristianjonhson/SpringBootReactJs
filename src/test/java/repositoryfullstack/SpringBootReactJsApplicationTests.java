98f487e0a5ab3aafb26aac8463aebd49f34b177f
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2021-09-05 07:09:22 -0300

    Initial commit

---
+++ src/test/java/repositoryfullstack/SpringBootReactJsApplicationTests.java
@@ -0,0 +1,13 @@
+package repositoryfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactJsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
